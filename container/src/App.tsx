b31eff82f1c419ab208eb6fcdafadae7d0822809
Phil Alsford <palsford@linz.govt.nz>
2022-04-28 13:22:41 +1200

    Store user on window

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Suspense } from "react";
 import "./App.css";
 const Header = React.lazy(() => import("@mpui/Header"));
@@ -6,43 +6,66 @@ const Squad1App = React.lazy(() => import("@squad1/App"));
 const Squad2App = React.lazy(() => import("@squad2/App"));
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+declare global {
+  interface Window {
+    theUser: any;
+  }
+}
+
 function App() {
+  const [user, setUser] = React.useState(window.theUser); // Replace with a hook which syncs with window or something
+
+  const handleUserChange = (event: any) => {
+    setUser(window.theUser);
+  };
+
+  useEffect(() => {
+    // just here to re-render
+    window.addEventListener("userChange", handleUserChange);
+
+    return () => {
+      window.removeEventListener("userChange", handleUserChange);
+    };
+  }, [user]);
+
   return (
     <div className="container">
       <BrowserRouter>
         <Suspense fallback={"loading..."}>
           <Header />
         </Suspense>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Suspense fallback={"loading..."}>
-                <>
-                  <h1>Home is a composition of two apps</h1>
+        {window.theUser && (
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={"loading..."}>
+                  <>
+                    <h1>Home is a composition of two apps</h1>
+                    <Squad1App />
+                    <Squad2App />
+                  </>
+                </Suspense>
+              }
+            />
+            <Route
+              path="/dealing"
+              element={
+                <Suspense fallback={"loading..."}>
                   <Squad1App />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/survey"
+              element={
+                <Suspense fallback={"loading..."}>
                   <Squad2App />
-                </>
-              </Suspense>
-            }
-          />
-          <Route
-            path="/dealing"
-            element={
-              <Suspense fallback={"loading..."}>
-                <Squad1App />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/survey"
-            element={
-              <Suspense fallback={"loading..."}>
-                <Squad2App />
-              </Suspense>
-            }
-          />
-        </Routes>
+                </Suspense>
+              }
+            />
+          </Routes>
+        )}
       </BrowserRouter>
     </div>
   );
--- header/src/Header.tsx
+++ header/src/Header.tsx
@@ -1,13 +1,59 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
 import "./Header.css";
 
+declare global {
+  interface Window {
+    theUser: any;
+  }
+}
+
+function login() {
+  window.theUser = {
+    id: "phil",
+    name: "Phil",
+    permissions: ["admin", "user"],
+  };
+
+  const customEvent = new CustomEvent("userChange", {
+    detail: {
+      id: "phil",
+      name: "Phil",
+      permissions: ["admin", "user"],
+    },
+  });
+  window.dispatchEvent(customEvent);
+}
+
+function logout() {
+  window.theUser = undefined;
+
+  const customEvent = new CustomEvent("userChange", {
+    detail: undefined,
+  });
+  window.dispatchEvent(customEvent);
+}
+
 export default function () {
+  const [user, setUser] = React.useState(window.theUser); // Replace with a hook which syncs with window or something
+
+  const handleUserChange = (event: any) => {
+    setUser(window.theUser);
+  };
+
+  useEffect(() => {
+    window.addEventListener("userChange", handleUserChange);
+
+    return () => {
+      window.removeEventListener("userChange", handleUserChange);
+    };
+  }, [user]);
+
   return (
     <header>
       <div className="header">
         <a href="#default" className="logo">
-          CompanyLogo
+          Current User: {window.theUser?.name}
         </a>
         <div className="header-right">
           <Link className="active" to="/">
@@ -15,6 +61,26 @@ export default function () {
           </Link>
           <Link to="/dealing">Dealing</Link>
           <Link to="/survey">Survey</Link>
+          {window.theUser ? (
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                logout();
+              }}
+            >
+              Logout
+            </a>
+          ) : (
+            <a
+              href="#default"
+              onClick={(e) => {
+                e.preventDefault();
+                login();
+              }}
+            >
+              Login
+            </a>
+          )}
         </div>
       </div>
     </header>
